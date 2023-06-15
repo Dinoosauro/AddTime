d0dc93b880887c91d88e96efee4a463ad69ebb2a
Dinoosauro <80783030+Dinoosauro@users.noreply.github.com>
2023-06-15 17:20:56 +0200

    Add documentation & webpage

--- node/script.js
+++ node/script.js
@@ -11,47 +11,46 @@ console.table({
     ]
 })
 function logicPrompt(prompt) {
-    if (!prompt.match(/^\d/) && prompt.length < 2 && prompt !== "q") return;
+    if (!prompt.match(/^\d/) && prompt.length < 2 && prompt !== "q") return; // If prompt isn't a number or it's too short and it isn't the command to exit the process, don't add anything.
     switch (prompt.substring(0, 1)) {
-        case "h":
+        case "h": // Hours
             seconds += parseInt(prompt.substring(1)) * 3600;
             break;
-        case "m":
+        case "m": // Minutes
             seconds += parseInt(prompt.substring(1)) * 60;
             break;
-        case "q":
+        case "q": // Quit process
             require("process").exit();
             break;
-        case "o":
+        case "o": // Change output
             switch (prompt.substring(1, 2)) {
-                case "m":
+                case "m": // in minutes
                     output = 1;
                     break;
-                case "h":
+                case "h": // in hours
                     output = 2;
                     break;
-                default:
+                default: // in seconds
                     output = 0;
                     break;
             }
         default:
-            if (prompt.match(/^\d/)) seconds += parseInt(prompt); else if (prompt.substring(0, 1).match(/^\d/)) seconds += parseInt(prompt.substring(1));
+            if (prompt.match(/^\d/)) seconds += parseInt(prompt); else if (prompt.substring(0, 1).match(/^\d/)) seconds += parseInt(prompt.substring(1)); // If it starts with a number, add it as seconds. If there's a letter before that, don't consider it and add it as seconds.
             break;
     }
 
 }
 let prompt = require("prompt-sync")();
 function askPrompt() {
-    logicPrompt(prompt("").toLowerCase());
+    logicPrompt(prompt("").toLowerCase()); // Add the time
     switch (output) {
-        case 0:
+        case 0: // Output in seconds
             console.log(`${seconds} seconds`);
             break;
-        case 1:
-            console.log(seconds);
+        case 1: // Output in minutes
             console.log(`${Math.floor(seconds / 60)} minutes and ${parseInt(seconds) - (Math.floor(parseInt(seconds) / 60) * 60)} seconds [${seconds} seconds...]`);
             break;
-        case 2:
+        case 2: // Output in hours
             console.log(`${Math.floor(seconds / 3600)} hours, ${Math.floor((seconds - (Math.floor(seconds / 3600) * 3600)) / 60)} minutes and ${seconds - (Math.floor(seconds / 60) * 60)} seconds [${seconds} seconds...]`);
             break;
     }
